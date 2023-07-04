10a6058a5e564ac0fe7c736e46b7dd4faa2f9e12
cryptojunkie <mrcholo21@icloud.com>
2023-07-04 20:30:37 +0100

    edit fetch

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -9,7 +9,7 @@ export default function InputComp() {
   const onSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/", {
+      const response = await fetch("https://scissor-api-9l4p.onrender.com/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
